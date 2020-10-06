bdd5a4957a6afd01c50538a76e1fd8fa5907e1c3
davincikab <davidnganganjeri079@gmail.com>
2020-10-06 09:06:14 +0300

    updated navigation bar box shadow'

--- road_monitor/urls.py
+++ road_monitor/urls.py
@@ -6,3 +6,7 @@ urlpatterns = [
     path("", include("roads.urls")),
     path("user/", include("user.urls")),
 ]
+
+admin.site.site_header = "Road Monitor"
+admin.site.site_title = "Road Monitor Administration"
+admin.site.index_title = "Road Monitor"
